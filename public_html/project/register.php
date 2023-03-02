9816d2080bd6f8fa45db8c7d0ae7d4ce45905446
Nick Fortis <fortisn7@gmail.com>
2023-03-02 12:04:10 -0500

    Finished Feat-FirstHelperFunctions

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -62,9 +62,14 @@
             $hasError = true;
         }
         // sanitize email
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        // $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $email = sanitize_email($email);
         // validate email
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Invalid email address";
+            $hasError = true;
+        }*/
+        if (!is_valid_email($email)) {
             echo "Invalid email address";
             $hasError = true;
         }
