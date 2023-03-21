d0e5f6027f006f8f2ffc3f2605145702489979e5
Nick Fortis <fortisn7@gmail.com>
2023-03-21 12:50:38 -0400

    finished feat-flashmessages

--- lib/functions.php
+++ lib/functions.php
@@ -13,4 +13,5 @@ require_once(__DIR__ . "/sanitizers.php");
 //TODO 3: User helpers
 require_once(__DIR__ . "/user_helpers.php");
 //TODO 4: Flash Message Helpers
+require_once(__DIR__ . "/flash_messages.php");
 ?>
